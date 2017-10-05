30244d2ed22c0e7452a15e0682b2097024700329
Ryan Felton <rfelton@greengoblin.local>
2017-10-05 15:56:47 -0400

    Better reflections, slight lighting change

--- triangle.fs
+++ triangle.fs
@@ -4,7 +4,7 @@ precision mediump float;
 
 const int MAX_MARCHING_STEPS = 255;
 const float MIN_DIST = 0.0;
-const float MAX_DIST = 150.0;
+const float MAX_DIST = 1500.0;
 const float EPSILON = 0.0001;
 
 uniform vec2 u_resolution;
@@ -35,7 +35,7 @@ struct primitive {
 };
 
 //global background color declaration
-vec3 background = vec3(0.8,0.8,0.9);
+vec3 background = vec3(0.75,0.8,0.95);
 
 /*
     Checks if the point is inside or outside of
@@ -74,16 +74,6 @@ float opTwistTorus( vec3 p )
     return sdTorus(q , vec2(1.0,0.25));
 }
 
-float smin( float a, float b, float k )
-{
-    float h = clamp( 0.5+0.5*(b-a)/k, 0.0, 1.0 );
-    return mix( b, a, h ) - k*h*(1.0-h);
-}
-
-float smoothUnion( float d1, float d2 )
-{
-    return smin(d1,d2, 0.5);
-}
 
 float un(float d1, float d2) {
     return min(d1,d2);
@@ -95,19 +85,19 @@ float sceneSDF(vec3 pt) {
 
     float floor = sdBox(pt-vec3(0,-1.7,0), vec3(30,0.0,20.0));
     float sphere = sdSphere(pt-vec3(sin(u_time),-0.5+(sin(u_time)*0.3),cos(u_time)), 0.5);
-    float mirrorSphere = sdSphere(pt-vec3(2.811,1.0,-3.0), 2.0);
-    float mirrorCube = sdBox(pt-vec3(-1.2,0.869,-1.712), vec3(1.0,1.0,1.0));
-    float mirrorTorus = sdTorus(pt-vec3(0,0.5,0.0),vec2(12.0,1.5));
+    float mirrorSphere = sdSphere(pt-vec3(2.811,1.0+(sin(u_time)*0.3),-3.0), 2.0);
+    float mirrorCube = sdBox(pt-vec3(-3.5,0.869,-1.712), vec3(1.0,6.0,2.0));
+    //float mirrorTorus = sdTorus(pt-vec3(0,0.5,0.0),vec2(12.0,1.5));
     float twistTorus = opTwistTorus(pt- vec3(-1.5,0.5,-4.0));
 
 
-    float value = un(un(un(floor, sphere),un(mirrorTorus, mirrorSphere)),twistTorus);
+    float value = un(un(un(floor, sphere),un(mirrorCube, mirrorSphere)),twistTorus);
 
     if(value == floor) {
         ray.hit = 0;
-    } else if (value == sphere || value == twistTorus) {
+    } else if (value == sphere || value == twistTorus ) {
         ray.hit = 1;
-    } else if (value == mirrorSphere || value == mirrorTorus) {
+    } else if (value == mirrorCube || value == mirrorSphere) {
         ray.hit = 2;
     }
 
@@ -156,14 +146,13 @@ vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
     return normalize(vec3(xy,-z));
 }
 
-vec3 estimateNormal(vec3 pt) {
+vec3 estimateNormal(vec3 p) {
     return normalize(vec3(
-        sceneSDF(vec3(pt.x+EPSILON, pt.yz)) - sceneSDF(vec3(pt.x-EPSILON,pt.yz)),
-        sceneSDF(vec3(pt.x, pt.y+EPSILON, pt.z)) - sceneSDF(vec3(pt.x,pt.y-EPSILON,pt.z)),
-        sceneSDF(vec3(pt.xy,pt.z+EPSILON)) - sceneSDF(vec3(pt.xy, pt.z - EPSILON))
+        sceneSDF(vec3(p.x + EPSILON, p.y, p.z)) - sceneSDF(vec3(p.x - EPSILON, p.y, p.z)),
+        sceneSDF(vec3(p.x, p.y + EPSILON, p.z)) - sceneSDF(vec3(p.x, p.y - EPSILON, p.z)),
+        sceneSDF(vec3(p.x, p.y, p.z  + EPSILON)) - sceneSDF(vec3(p.x, p.y, p.z - EPSILON))
     ));
 }
-
 //lighting start
 vec3 diffuseLighting(vec3 pt, light currentLight, vec3 normal) {
     vec3 lightDir = normalize(currentLight.pos-pt);
@@ -194,7 +183,7 @@ vec3 floorCheckerboard(vec3 pt) {
 
 bool shadow(vec3 pt, light currentLight) {
 
-    float dist = shortestDistanceToSurface(pt, normalize(currentLight.pos - pt), MIN_DIST+ 0.001, length(currentLight.pos - pt));
+    float dist = shortestDistanceToSurface(pt, normalize(currentLight.pos - pt), MIN_DIST+ 0.01, length(currentLight.pos - pt));
 
     bool hit;
     if (dist < length(currentLight.pos - pt)) {
@@ -220,7 +209,7 @@ vec3 lighting(vec3 pt, vec3 eye, light currentLight, vec3 objectColor) {
 
 
     if (shadow) {
-        ptColor = (float(shadow) * vec3(-0.8, -0.8, -0.1) + ambientLight) * objectColor;
+        ptColor = (float(shadow) * vec3(-0.1, -0.1, -0.1) + ambientLight) * objectColor;
     }
 
     return ptColor;
@@ -239,18 +228,19 @@ vec3 getObjectColor(vec3 pt) {
     return objectColor;
 }
 
+/*
 vec3 mirror(vec3 pt, vec3 eye, light currentLight) {
     vec3 objectColor, normal, v, reflectedV;
     float dist, noHit;
-    vec3 mirrorColor = vec3(-0.0);
+    vec3 mirrorColor = vec3(1.0, 1.0, 1.0);
 
-    const int numBounces = 6;
+    const int numBounces = 14;
     int bounces = 0;
     for (int i = 0; i < numBounces; i ++) {
         normal = estimateNormal(pt);
-        v = normalize(eye - pt);
+        v = eye - pt;
         reflectedV = normalize(reflect(-v, normal));
-        dist = shortestDistanceToSurface(pt, reflectedV, MIN_DIST + 0.0001, MAX_DIST);
+        dist = shortestDistanceToSurface(pt, reflectedV, MIN_DIST + 0.001, MAX_DIST);
         noHit = step(dist, MAX_DIST - EPSILON);
 
         pt = pt + (normalize(reflectedV) * dist);
@@ -264,9 +254,40 @@ vec3 mirror(vec3 pt, vec3 eye, light currentLight) {
         objectColor = lighting(pt, reflectedV, currentLight, vec3(0.6));
     }
 
-    vec3 reflectionColor = ((1.0-noHit) * (background + mirrorColor)) + (noHit * (objectColor + mirrorColor));
+    vec3 reflectionColor = ((1.0-noHit) * (background)) + (noHit * (objectColor));
 
     return lighting(pt, eye, currentLight, reflectionColor);
+}
+*/
+
+vec3 mirror(vec3 pt, vec3 eye, light currentLight) {
+    vec3 objectColor, normal, v, reflectedV, ogpt;
+    float dist, noHit;
+    vec3 mirrorColor = vec3(1.0, 1.0, 1.0);
+    ogpt = pt;
+
+    const int numBounces = 4;
+    int bounces = 0;
+    for (int i = 0; i < numBounces; i ++) {
+        if(ray.hit == 0 || ray.hit == 1) {
+            objectColor = lighting(pt, reflectedV, currentLight, getObjectColor(pt));
+            break;
+        }
+        normal = estimateNormal(pt);
+        v = eye - pt;
+        reflectedV = normalize(reflect(-v, normal));
+        dist = shortestDistanceToSurface(pt, reflectedV, MIN_DIST + 0.001, MAX_DIST);
+        noHit = step(dist, MAX_DIST - EPSILON);
+
+        pt += (normalize(reflectedV) * dist);
+    }
+    if (bounces == numBounces-1) {
+        objectColor = lighting(pt, reflectedV, currentLight, vec3(0.6));
+    }
+
+    vec3 reflectionColor = ((1.0-noHit) * (background)) + (noHit * (objectColor));
+
+    return lighting(ogpt, eye, currentLight, reflectionColor);
 }
 
 vec3 lightingStyle(vec3 pt, vec3 eye, light currentLight) {
@@ -301,8 +322,8 @@ void main() {
     //ray.direction = rayDirection(75.0, u_resolution.xy, gl_FragCoord.xy);
     //vec3 eye = vec3(0,-0.2 + sin(u_time/3.0)*0.5, sin(u_time)*1.0 + 9.0);
 
-    vec3 eye = vec3(0, 5.0, 0.0);
-    vec3 at = vec3(0);
+    vec3 eye = vec3(0, 3.0, 0.0);
+    vec3 at = vec3(0,2.7,0);
     eye.x += 14.7 * sin(u_time * 0.2);
     eye.z += 14.7 * cos(u_time * 0.2);
     vec2 uv = gl_FragCoord.xy/u_resolution.xy;
@@ -320,10 +341,10 @@ void main() {
 
     //start lighting
     light mainlight;
-    mainlight.pos = vec3(cos(u_time),0.5+(cos(u_time)*0.3),sin(u_time));
+    mainlight.pos = vec3(0,6.5,0);
     mainlight.specularColor = vec3(0.9,0.9,0.9);
-    mainlight.diffuseColor = vec3(0.75,0.75,0.75);
-    mainlight.ambientColor = vec3(0.3,0.3,0.3);
+    mainlight.diffuseColor = vec3(0.7);
+    mainlight.ambientColor = vec3(0.5);
     mainlight.intensity = 0.8;
 
     vec3 lightingColor = lightingStyle(pt, eye, mainlight);
